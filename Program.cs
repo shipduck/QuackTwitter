d92dfd46e3626c03db281c7d700ac9738e273f72
sapphiredev <se.imas.kr@gmail.com>
2015-02-18 08:37:09 +0900

    changed to tab indentation

--- Program.cs
+++ Program.cs
@@ -8,26 +8,26 @@ using QuackTwitter;
 
 namespace QuackTest
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var twitter = new Twitter();
-            twitter.Authenticate();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var twitter = new Twitter();
+			twitter.Authenticate();
 
-            var parameters = new Dictionary<String, String>();
-            parameters.Add("count", "2");
-            var json = twitter.REST(Twitter.Statuses.UserTimeline, parameters);
-            foreach (var tweet in json)
-            {
-                Console.WriteLine(tweet.text);
-            }
-            parameters.Clear();
-            parameters.Add("status", "hello world");
-            json = twitter.REST(Twitter.Statuses.Update, parameters);
-            Console.WriteLine(json);
+			var parameters = new Dictionary<String, String>();
+			parameters.Add("count", "2");
+			var json = twitter.REST(Twitter.Statuses.UserTimeline, parameters);
+			foreach (var tweet in json)
+			{
+				Console.WriteLine(tweet.text);
+			}
+			parameters.Clear();
+			parameters.Add("status", "hello world");
+			json = twitter.REST(Twitter.Statuses.Update, parameters);
+			Console.WriteLine(json);
 
-            Console.ReadLine();
-        }
-    }
+			Console.ReadLine();
+		}
+	}
 }
--- QuackTwitter/Constants.cs
+++ QuackTwitter/Constants.cs
@@ -2,27 +2,27 @@
 
 namespace QuackTwitter
 {
-    class Constants
-    {
-        public const String BaseUrl = "https://api.twitter.com/1.1";
-        public const String OAuthUrl = "https://api.twitter.com/oauth/authorize";
-        public const String StatusesUrl = BaseUrl + "/statuses";
-        public const String MediaUrl = "https://upload.twitter.com/1.1/media";
-        public const String DirectMessagesUrl = BaseUrl + "/direct_messages";
-        public const String SearchUrl = BaseUrl + "/search";
-        public const String FriendshipsUrl = BaseUrl + "/friendships";
-        public const String FriendsUrl = BaseUrl + "/friends";
-        public const String FollowersUrl = BaseUrl + "/followers";
-        public const String AccountUrl = BaseUrl + "/account";
-        public const String BlocksUrl = BaseUrl + "/blocks";
-        public const String UsersUrl = BaseUrl + "/users";
-        public const String MutesUrl = BaseUrl + "/mutes";
-        public const String FavoritesUrl = BaseUrl + "/favorites";
-        public const String ListsUrl = BaseUrl + "/lists";
-        public const String SavedSearchesUrl = BaseUrl + "/saved_searches";
-        public const String GeoUrl = BaseUrl + "/geo";
-        public const String TrendsUrl = BaseUrl + "/trends";
-        public const String ApplicationUrl = BaseUrl + "/application";
-        public const String HelpUrl = BaseUrl + "/help";
-    }
+	class Constants
+	{
+		public const String BaseUrl = "https://api.twitter.com/1.1";
+		public const String OAuthUrl = "https://api.twitter.com/oauth/authorize";
+		public const String StatusesUrl = BaseUrl + "/statuses";
+		public const String MediaUrl = "https://upload.twitter.com/1.1/media";
+		public const String DirectMessagesUrl = BaseUrl + "/direct_messages";
+		public const String SearchUrl = BaseUrl + "/search";
+		public const String FriendshipsUrl = BaseUrl + "/friendships";
+		public const String FriendsUrl = BaseUrl + "/friends";
+		public const String FollowersUrl = BaseUrl + "/followers";
+		public const String AccountUrl = BaseUrl + "/account";
+		public const String BlocksUrl = BaseUrl + "/blocks";
+		public const String UsersUrl = BaseUrl + "/users";
+		public const String MutesUrl = BaseUrl + "/mutes";
+		public const String FavoritesUrl = BaseUrl + "/favorites";
+		public const String ListsUrl = BaseUrl + "/lists";
+		public const String SavedSearchesUrl = BaseUrl + "/saved_searches";
+		public const String GeoUrl = BaseUrl + "/geo";
+		public const String TrendsUrl = BaseUrl + "/trends";
+		public const String ApplicationUrl = BaseUrl + "/application";
+		public const String HelpUrl = BaseUrl + "/help";
+	}
 }
